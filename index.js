384d53b61be374588365c261fd8fc0eaa0268b10
Angel Baro <angelbaro44@gmail.com>
2021-02-25 15:11:09 -0500

    added alt alt solution

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ var summation = function (num) {
 
 //I was contiplating the math and found an easier solution i remembered from highschool!
 // (Plus this is a faster solution)
-function summation(num) {
+function summation2(num) {
       return num * (num + 1) / 2
     } 
+
+ //Test Cases
+ console.log(summation2(1)) //Expected Result: 1
+ console.log(summation2(8)) //Expected Result: 36
+ console.log(summation2(22)) //Expected Result: 253
+ console.log(summation2(100)) //Expected Result: 5050
+ console.log(summation2(213)) //Expected Result: 22791
+
+ //or
+const summation3 = n => n * (n + 1) / 2;
+
+ //Test Cases
+ console.log(summation3(1)) //Expected Result: 1
+ console.log(summation3(8)) //Expected Result: 36
+ console.log(summation3(22)) //Expected Result: 253
+ console.log(summation3(100)) //Expected Result: 5050
+ console.log(summation3(213)) //Expected Result: 22791
