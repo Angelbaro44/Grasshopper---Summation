e98483a4db9c2ed0fcdb6379b8bd6d4f09c77db8
Angel Baro <angelbaro44@gmail.com>
2021-02-25 15:02:59 -0500

    finish kata

---
+++ index.js
@@ -0,0 +1,10 @@
+//This was my init answer but this gets refined later on :)
+
+var summation = function (num) {
+      let result = 0;
+      for (var i = 1; i <= num; i++) {
+        result += i;
+      }
+      
+      return result;
+    }
